074db0d59cd9433b010406c59b24c13727689a8a
Pavel Naumenko <ghostblade3301@gmail.com>
2023-09-23 22:59:43 +0300

    added variables and pep function

---
+++ src/constants.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+PEP_INDEX_URL = 'https://peps.python.org/'
+MAIN_DOC_URL = 'https://docs.python.org/3/'
+BASE_DIR = Path(__file__).parent
+DOWNLOADS_DIR = BASE_DIR / 'downloads'
+DATETIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
+EXPECTED_STATUS = {
+    'A': ['Active', 'Accepted'],
+    'D': ['Deferred'],
+    'F': ['Final'],
+    'P': ['Provisional'],
+    'R': ['Rejected'],
+    'S': ['Superseded'],
+    'W': ['Withdrawn'],
+    '': ['Draft', 'Active'],
+}
