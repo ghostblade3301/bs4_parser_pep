01e2d570a8c4b7dca0f7d02bb1455096ca4f511b
Pavel Naumenko <pavelnaumenkowork@gmail.com>
2023-09-25 09:53:24 +0300

    review_v3

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,3 +1,9 @@
+# exceptions.py
+class ParserFindTagException(Exception):
+    '''Вызывается, когда парсер не может найти тег.'''
+    pass
+
+
 class ParserFindUrlException(Exception):
     '''Вызывается, когда парсер не смог найти в тэгах
     блок со ссылками на документацию'''
